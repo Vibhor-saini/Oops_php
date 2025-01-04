740e62d03def8cd39aee296f9b10288c821632ab
Vibhor saini <98160883+Vibhor-saini@users.noreply.github.com>
2025-01-04 18:06:29 +0530

    created another object

--- class_obj.php
+++ class_obj.php
@@ -23,6 +23,9 @@ $obj1= new myname();
 $obj1->Secondname("Sugandha Saini");
 
 $obj2 = new myname();
-$obj2->Secondname("Sugandha Saini");
+$obj2->Secondname("Mayank Saini");
+
+$obj3 = new myname();
+$obj3->Secondname("Anubhav Saini");
 
 ?>
